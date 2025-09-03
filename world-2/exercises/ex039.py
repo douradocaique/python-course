9a5856fd9d6270a6206d67f39a18f570c9cbfce0
Dourado Caique <cdrd.dev@gmail.com>
2025-09-03 17:12:35 -0300

    :sparkles: Refactor military service enlistment logic for clarity and structure

--- world-2/exercises/ex039.py
+++ world-2/exercises/ex039.py
@@ -4,11 +4,10 @@ birth_year = int(input('Enter your birth year: '))
 year_now = date.today().year
 age = year_now - birth_year
 
-if(age>=18):
-    print('This is the time for your enlistment')
-    if age>18:
-        print('You have exceeded your enlistment time in {} years, go to one of our posts'.format(age-18))
-    elif age == 18:
-        print('Congratulations, you are at the ideal age to enlist.')
-else:
+
+if age == 18:
+    print('Congratulations, you are at the ideal age to enlist.')
+elif age > 18:
+    print('You have exceeded your enlistment time in {} years, go to one of our posts'.format(age-18))
+elif age < 18:
     print('There are still {} year(s) to go'.format(18-age))
